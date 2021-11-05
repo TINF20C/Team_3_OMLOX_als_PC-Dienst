09a104c9d1c2db758aaa19df999ed149be370706
Samir Ahmetovic <samir-a@t-online.de>
2021-11-05 15:22:03 +0100

    API test

--- SOURCECODE/OmloxBackend/OmloxBackend/DeepHub.cs
+++ SOURCECODE/OmloxBackend/OmloxBackend/DeepHub.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace OmloxBackend
 {
-    internal class Class1
+    internal class DeepHub
     {
+        HttpClient httpClient = new HttpClient();
+        public void function()
+        {
+            //https://stackoverflow.com/questions/9620278/how-do-i-make-calls-to-a-rest-api-using-c
+            httpClient.BaseAddress = new Uri("https://api.deephub.io/deephub/v1/");
+            APIReq req = new APIReq(httpClient);
+            Console.WriteLine(req.GetAllTrackableIdsAsync().GetAwaiter().GetResult());
+        }
+
     }
 }
