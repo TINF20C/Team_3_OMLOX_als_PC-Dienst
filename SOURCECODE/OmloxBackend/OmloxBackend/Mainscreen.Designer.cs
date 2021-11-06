43dbf795f30db2b9557efb83e2cc604cb0963417
AaronWeis <tuba.aaron@gmail.com>
2021-11-06 15:17:07 +0100

    Add Device List and  Action Buttons to Mainscreen

--- SOURCECODE/OmloxBackend/OmloxBackend/Mainscreen.Designer.cs
+++ SOURCECODE/OmloxBackend/OmloxBackend/Mainscreen.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.logoutButton = new System.Windows.Forms.Button();
+            this.deviceList = new System.Windows.Forms.CheckedListBox();
+            this.deviceAddButton = new System.Windows.Forms.Button();
+            this.deviceRemoveButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // logoutButton
@@ -41,14 +45,62 @@
             this.logoutButton.UseVisualStyleBackColor = true;
             this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
             // 
+            // deviceList
+            // 
+            this.deviceList.CheckOnClick = true;
+            this.deviceList.FormattingEnabled = true;
+            this.deviceList.Items.AddRange(new object[] {
+            "device 1",
+            "device 2"});
+            this.deviceList.Location = new System.Drawing.Point(26, 30);
+            this.deviceList.Name = "deviceList";
+            this.deviceList.Size = new System.Drawing.Size(304, 364);
+            this.deviceList.TabIndex = 2;
+            // 
+            // deviceAddButton
+            // 
+            this.deviceAddButton.Location = new System.Drawing.Point(337, 30);
+            this.deviceAddButton.Name = "deviceAddButton";
+            this.deviceAddButton.Size = new System.Drawing.Size(75, 23);
+            this.deviceAddButton.TabIndex = 3;
+            this.deviceAddButton.Text = "Add Device";
+            this.deviceAddButton.UseVisualStyleBackColor = true;
+            this.deviceAddButton.Click += new System.EventHandler(this.deviceAddButton_Click);
+            // 
+            // deviceRemoveButton
+            // 
+            this.deviceRemoveButton.Location = new System.Drawing.Point(337, 60);
+            this.deviceRemoveButton.Name = "deviceRemoveButton";
+            this.deviceRemoveButton.Size = new System.Drawing.Size(75, 23);
+            this.deviceRemoveButton.TabIndex = 4;
+            this.deviceRemoveButton.Text = "Remove Device";
+            this.deviceRemoveButton.UseVisualStyleBackColor = true;
+            this.deviceRemoveButton.Click += new System.EventHandler(this.deviceRemoveButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(337, 90);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 5;
+            this.showButton.Text = "Show";
+            this.showButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
             // Mainscreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.deviceRemoveButton);
+            this.Controls.Add(this.deviceAddButton);
+            this.Controls.Add(this.deviceList);
             this.Controls.Add(this.logoutButton);
             this.Name = "Mainscreen";
             this.Text = "Mainscreen";
+            this.Load += new System.EventHandler(this.Mainscreen_Load);
             this.ResumeLayout(false);
 
         }
@@ -56,5 +108,9 @@
         #endregion
 
         private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.CheckedListBox deviceList;
+        private System.Windows.Forms.Button deviceAddButton;
+        private System.Windows.Forms.Button deviceRemoveButton;
+        private System.Windows.Forms.Button showButton;
     }
 }
